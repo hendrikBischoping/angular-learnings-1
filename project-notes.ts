a5a0cea6d147eb15d06ef9f8d9c15874dd0b46a3
hendrikBischoping <h.bischoping@gmx.de>
2025-04-27 14:13:38 +0200

    first commit

---
+++ project-notes.ts
@@ -0,0 +1,51 @@
+//  To install and use Angular you at first need to install nodeJS (LTS)
+//  
+//  Angular CLI
+//      Angular CLI is the 'command line interface' powered by node js
+//      it is used to for example install Angular itself globally on your device 
+//      (npm install - @angular/cli)
+//      
+//  Create an new Angular-project:
+//      type into powershell => ng new name-of-the-new-project
+//  
+//  
+//  Angular Essentials:
+//      - understanding Angular project structure
+//      - working with components
+//      - handling user events
+//      - rendering and updating dynamic UI content
+//  
+//  all project-dependencies are saved in the 'package.json' file
+//  you can install them at once by typing into powershell => npm install
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
+//  
