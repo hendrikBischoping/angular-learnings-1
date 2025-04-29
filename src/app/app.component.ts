5d55f0e9bdd194e2dfe5385038e8224ef537dfd7
hendrikBischoping <h.bischoping@gmx.de>
2025-04-29 15:09:33 +0200

    added header component (step by step)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent], //registers all 'stand-alone'-components that have to be used in this component
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -3,8 +3,10 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-header', //descripes what HTML-elements should be replaced by this component (at least 'two-words' to avoid overwrite 'build-in-elements' e.g. 'header')
     standalone: true, // 'true' is default in angular 19 and higher. in lower versions 'false' is default ('module-based' component)
-    templateUrl: './header.component.html' // relative path to find the used html content
+    templateUrl: './header.component.html', // relative path to find the used html content
     // template: '<h1>My Headline</h1> =>> alternative (in-line-approach) for very small html-code
+    styleUrl: './header.component.scss' //relative path to find the used SCSS content (Array of paths also possible: styleUrls: ['./path1', './path2'])
+    // styles: ['h1 {color: red}'] - inline-style barely used in rl
 })
 
-export class HeaderComponent {} // makes the component useable in other components as class
+export class HeaderComponent {} // makes the component useable in other components as class 
