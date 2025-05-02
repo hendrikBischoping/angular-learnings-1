946b7c420a143d89cba11953d4d1a55b407ead5d
hendrikBischoping <h.bischoping@gmx.de>
2025-05-02 13:05:53 +0200

    added 'dummy-user'-array + rendering one user randomised

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header.component'
+import { HeaderComponent } from './header/header.component'
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent], //registers all 'stand-alone'-components that have to be used in this component
+  imports: [HeaderComponent, UserComponent], //registers all 'stand-alone'-components that have to be used in this component
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
