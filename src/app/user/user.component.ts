79cf7f9b4a2bf9d3d54ae233752aaf4d90a3c37e
hendrikBischoping <h.bischoping@gmx.de>
2025-05-04 15:30:31 +0200

    toggle rendered user on click (

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -13,8 +13,15 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 })
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex];
-
+  
   get imgPth() { // 'getter' - a method that NOT need to be called / executed (can be used like a property). returns a new value (e.g. dynamical)
     return 'assets/users/' + this.selectedUser.avatar;
+  }
+  
+  onSelectUser() { // 'on...' is a common way to name methods/functions that get executed on events
+    console.log('Clicked!');
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+    this.selectedUser = DUMMY_USERS[randomIndex];
+    
   }
 }
