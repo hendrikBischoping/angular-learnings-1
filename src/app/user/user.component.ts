3b5df5925c5750dffaf5b6f3adfa46337541e856
hendrikBischoping <h.bischoping@gmx.de>
2025-05-13 23:03:51 +0200

    used @Input-decorator to set propertiey as required

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-user';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,12 +8,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  @Input({required: true}) avatar!: string; //'decoratores (@...) can be added to classes BUT ALSO to properties within classes!
+  @Input({required: true}) name!: string; // '{required: true}' is a configuration-option to set the property 'name' (and 'avatar') as 'required' (angular throws an error if the properts isn`t set!)
+
+  get imagePath() {
+     return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
-    console.log('Clicked!');
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    
   }
 }
