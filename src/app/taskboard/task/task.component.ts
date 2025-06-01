f27079d1fa4b09d88a1fb986a24ad8840873d2e6
hendrikBischoping <h.bischoping@gmx.de>
2025-06-01 17:36:13 +0200

    using 'pipes' to set the layout of date (taks-component)

--- src/app/taskboard/task/task.component.ts
+++ src/app/taskboard/task/task.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DatePipe } from '@angular/common'; // pipe (made by angular) to transform the output of the date
 
 import { type Task } from './task.model'
 import { CardComponent } from "../../shared/card/card.component";
@@ -7,7 +8,7 @@ import { CardComponent } from "../../shared/card/card.component";
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe], // pipes need to be called in 'imports' to get available to templates
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
